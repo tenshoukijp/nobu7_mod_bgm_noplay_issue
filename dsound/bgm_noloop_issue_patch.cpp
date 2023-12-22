f26e5b31ef9a112bf6df2a5f0c964a374c2f1e1f
翔.jp <komiyama_a@hotmail.com>
2023-12-23 00:38:20 +0900

    4-11ではなく、5-12の修正。番号間違い。

--- dsound/bgm_noloop_issue_patch.cpp
+++ dsound/bgm_noloop_issue_patch.cpp
@@ -6,7 +6,7 @@ int bgm_noloop_issue_patch(int iBGMNo) {
 
 	// メイン画面に戻ってきた時、BGMが鳴らないことがあるのでパッ
 	// 4～11番はメイン画面の通常の地方BGM
-	if (4 <= iBGMNo && iBGMNo <= 11) {
+	if (5 <= iBGMNo && iBGMNo <= 12) {
 		nLastMainStrategyScreenBGM = iBGMNo;
 	}
 	// メイン画面に戻ってきた時、BGMが鳴らないことがあるのでパッチ
